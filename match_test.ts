6dd032c1553c860f08dd5ea37cb1c202ff75270d
Jon Anderson <jon.david.anderson@gmail.com>
2024-01-17 18:32:26 -0800

    Added test for dropping from queue.

--- match_test.ts
+++ match_test.ts
@@ -28,3 +28,31 @@ Deno.test("enqueue", () => {
     assertEquals(item1, "thing1");
     assertEquals(item2, "thing2");
 });
+
+Deno.test("drop", () => {
+    let item1: string | undefined;
+    let item2: string | undefined;
+
+    const m = new MatchMaker<string>(false, (i1, i2) => {
+        item1 = i1;
+        item2 = i2;
+    });
+
+    assertEquals(item1, undefined);
+    assertEquals(item2, undefined);
+
+    m.enqueue("1", "thing1")
+
+    assertEquals(item1, undefined);
+    assertEquals(item2, undefined);
+
+    m.drop("1")
+
+    assertEquals(item1, undefined);
+    assertEquals(item2, undefined);
+
+    m.enqueue("2", "thing2")
+
+    assertEquals(item1, undefined);
+    assertEquals(item2, undefined);
+})
